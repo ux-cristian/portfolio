98cf0ebbf86ca957b35da0c9f532ba6f80912c3f
ux-cristian <cristian08nc@gmail.com>
2025-05-23 17:51:19 -0500

    feat: rutas dinámicas para Project, Json para gestionar contenido y rutas de los proyectos

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    id: "tokens",
+    title: "Tokens Design System",
+    description: "Sistema de diseño basado en tokens",
+    technologies: ["React", "Node.js"],
+    details: {
+        year: 2023,
+        description: "Sistema de diseño basado en tokens para una mejor gestión de estilos y temas en aplicaciones web.",
+    }
+  },
+  {
+    id: "otro-proyecto",
+    title: "Otro Proyecto",
+    description: "Descripción bonita del proyecto",
+    technologies: ["React", "Node.js"],
+    details: {
+        year: 2025,
+        description: "Otro proyecto facherito para una mejor gestión de estilos y temas en aplicaciones web.",
+    }
+  },
+];
