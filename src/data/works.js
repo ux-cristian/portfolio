3c82f7af91eeed0cdb46ea6ed70c6f1545ac2e0b
ux-cristian <cristian08nc@gmail.com>
2025-05-24 11:43:55 -0500

    feat: contenido dinámico para las experiencias de trabajo

---
+++ src/data/works.js
@@ -0,0 +1,23 @@
+export const works = [
+    {
+        title: "Diseñador UX/UI",
+        company: "Eccocar",
+        startDate: "Junio 2024",
+        endDate: "Febrero 2025",
+        description: "Diseñé productos digitales en colaboración con empresas líderes del sector turístico, creando sistemas de diseño en Figma basados en Material Design para soluciones marca blanca. Optimicé la entrega a desarrollo mediante el uso de tokens, autolayout y documentación adaptada al marco Scrum.",
+    },
+    {
+        title: "Diseñador UX/UI",
+        company: "Reval SAS",
+        startDate: "Junio 2023",
+        endDate: "Abril 2024",
+        description: "Diseñé en colaboración con otros diseñadores una plataforma para la gestión de subsidios que mejoró los procesos de entrega y permitió escalar a nivel nacional. Documenté los prototipos en Figma usando autolayout, variables de color y definí un UI Kit para facilitar su implementación en desarrollo.",
+    },
+    {
+        title: "Diseñador UX/UI",
+        company: "Atix Digital",
+        startDate: "Enero 2023",
+        endDate: "Mayo 2023",
+        description: "Creé un UI Kit en Figma basado en Atomic Design para Notery y diseñé presentaciones comerciales para clientes potenciales de Atix. Como freelance en 2024, rediseñé la interfaz de P&C para mejorar la experiencia de usuario.",
+    },
+]
