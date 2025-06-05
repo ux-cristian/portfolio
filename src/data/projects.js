8aaa108a917bfbd4d95be8f5573b9d4e1c16c5d2
ux-cristian <cristian08nc@gmail.com>
2025-06-05 16:58:02 -0500

    refactor: optimización de multimedia y contenido

--- src/data/projects.js
+++ src/data/projects.js
@@ -21,17 +21,17 @@ export const projects = [
             {
                 title: "Proceso paso 2",
                 description: "Descripción del paso 2 del proceso de diseño.",
-                type: "image/png",
-                url: "/portfolio/projects/tokens/tokens_project_2.png",
+                type: "image/webp",
+                url: "/portfolio/media_pending.webp",
             }
         ]
       }
   },
   {
     id: "tokens-2",
-    cover_image: "/portfolio/projects/tokens/token_cover.jpg",
-    category: "Sistemas de diseño",
-    title: "Sistema de tokens",
+    cover_image: "/portfolio/media_pending.webp",
+    category: "Diseño UI",
+    title: "Rediseño P&C",
     description: "Tokens con las variables de Figma basado en el sistema de diseño de Material Design (google), para cambio dínamico de tema en aplicaciones de marca blanca de Eccocar.",
     details: {
         client: "Eccocar",
@@ -49,8 +49,8 @@ export const projects = [
             {
                 title: "Proceso paso 2",
                 description: "Descripción del paso 2 del proceso de diseño.",
-                type: "image/png",
-                url: "/portfolio/projects/tokens/tokens_project_2.png",
+                type: "image/webp",
+                url: "/portfolio/media_pending.webp",
             }
         ]
       }
