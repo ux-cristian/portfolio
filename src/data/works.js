d2260065fa2adff4628a00f67f1f6ea2bc42fef9
ux-cristian <cristian08nc@gmail.com>
2025-09-09 18:38:26 -0500

    content: se cambió las descripciones de los trabajos

--- src/data/works.js
+++ src/data/works.js
@@ -5,7 +5,7 @@ export const works = [
         urlCompany: "https://eccocar.com",
         startDate: "Junio 2024",
         endDate: "Febrero 2025",
-        description: "Diseñé productos digitales en colaboración con empresas líderes del sector turístico, creando sistemas de diseño en Figma basados en Material Design para soluciones marca blanca. Optimicé la entrega a desarrollo mediante el uso de tokens, autolayout y documentación adaptada al marco Scrum.",
+        description: "Colaboré en la construcción de productos digitales para empresas líderes del sector turístico. Usando Figma para el diseño de prototipos, en los cuales se aplicó tokens, autolayout y documentación adaptada al marco Scrum.",
     },
     {
         title: "Diseñador UX/UI",
@@ -13,7 +13,7 @@ export const works = [
         urlCompany: "https://www.reval.com.co/",
         startDate: "Junio 2023",
         endDate: "Abril 2024",
-        description: "Diseñé en colaboración con otros diseñadores una plataforma para la gestión de subsidios que mejoró los procesos de entrega y permitió escalar a nivel nacional. Documenté los prototipos en Figma usando autolayout, variables de color y definí un UI Kit para facilitar su implementación en desarrollo.",
+        description: "En colaboración con otros diseñadores, ideamos una aplicación móvil para la entrega de subsidios. También trabajamos en proyectos internos donde documenté los diseños en Figma y definí un UI Kit para facilitar su implementación en desarrollo.",
     },
     {
         title: "Diseñador UX/UI",
@@ -21,6 +21,6 @@ export const works = [
         urlCompany: "https://atixdigital.com/es/",
         startDate: "Enero 2023",
         endDate: "Mayo 2023",
-        description: "Creé un UI Kit en Figma basado en Atomic Design para Notery y diseñé presentaciones comerciales para clientes potenciales de Atix. Como freelance en 2024, rediseñé la interfaz de P&C para mejorar la experiencia de usuario.",
+        description: "Creé un UI Kit en Figma para la aplicación Notery y diseñé presentaciones comerciales para clientes potenciales de Atix. Como freelance en 2024, rediseñé la UX de la aplicacion SEIZ para un cliente.",
     },
 ]
